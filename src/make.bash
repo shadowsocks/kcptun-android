90b2b3847aa9211ab49fb4425803055057537eca
Max Lv <max.c.lv@gmail.com>
2017-05-19 12:21:26 +0800

    Fix the crash on Android O. #19

--- src/make.bash
+++ src/make.bash
@@ -4,26 +4,36 @@ function try () {
 "$@" || exit -1
 }
 
-[ -z "$ANDROID_NDK_HOME" ] && ANDROID_NDK_HOME=~/android-ndk-r12b
+[ -z "$ANDROID_NDK_HOME" ] && ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle
 
-DIR=$(pwd)/src
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MIN_API=$1
 DEPS=$(pwd)/.deps
 ANDROID_ARM_TOOLCHAIN=$DEPS/android-toolchain-${MIN_API}-arm
+ANDROID_ARM64_TOOLCHAIN=$DEPS/android-toolchain-21-arm64
 ANDROID_X86_TOOLCHAIN=$DEPS/android-toolchain-${MIN_API}-x86
 
-ANDROID_ARM_CC=$ANDROID_ARM_TOOLCHAIN/bin/arm-linux-androideabi-gcc
+ANDROID_ARM_CC=$ANDROID_ARM_TOOLCHAIN/bin/arm-linux-androideabi-clang
 ANDROID_ARM_STRIP=$ANDROID_ARM_TOOLCHAIN/bin/arm-linux-androideabi-strip
 
-ANDROID_X86_CC=$ANDROID_X86_TOOLCHAIN/bin/i686-linux-android-gcc
+ANDROID_ARM64_CC=$ANDROID_ARM64_TOOLCHAIN/bin/aarch64-linux-android-clang
+ANDROID_ARM64_STRIP=$ANDROID_ARM64_TOOLCHAIN/bin/aarch64-linux-android-strip
+
+ANDROID_X86_CC=$ANDROID_X86_TOOLCHAIN/bin/i686-linux-android-clang
 ANDROID_X86_STRIP=$ANDROID_X86_TOOLCHAIN/bin/i686-linux-android-strip
 
-try mkdir -p $DEPS $DIR/main/libs/armeabi-v7a $DIR/main/libs/x86
+try mkdir -p $DEPS $DIR/main/libs/armeabi-v7a $DIR/main/libs/x86 $DIR/main/libs/arm64-v8a
 
 if [ ! -d "$ANDROID_ARM_TOOLCHAIN" ]; then
     echo "Make standalone toolchain for ARM arch"
     $ANDROID_NDK_HOME/build/tools/make_standalone_toolchain.py --arch arm \
         --api $MIN_API --install-dir $ANDROID_ARM_TOOLCHAIN
+fi
+
+if [ ! -d "$ANDROID_ARM64_TOOLCHAIN" ]; then
+    echo "Make standalone toolchain for ARM64 arch"
+    $ANDROID_NDK_HOME/build/tools/make_standalone_toolchain.py --arch arm64 \
+        --api 21 --install-dir $ANDROID_ARM64_TOOLCHAIN
 fi
 
 if [ ! -d "$ANDROID_X86_TOOLCHAIN" ]; then
@@ -57,6 +67,11 @@ echo "Cross compile kcptun for arm"
 try env CGO_ENABLED=1 CC=$ANDROID_ARM_CC GOOS=android GOARCH=arm GOARM=7 go build -ldflags="-s -w"
 try $ANDROID_ARM_STRIP client
 try mv client $DIR/main/libs/armeabi-v7a/libkcptun.so
+
+echo "Cross compile kcptun for arm64"
+try env CGO_ENABLED=1 CC=$ANDROID_ARM64_CC GOOS=android GOARCH=arm64 go build -ldflags="-s -w"
+try $ANDROID_ARM64_STRIP client 
+try mv client $DIR/main/libs//arm64-v8a/libkcptun.so
 
 echo "Cross compile kcptun for x86"
 try env CGO_ENABLED=1 CC=$ANDROID_X86_CC GOOS=android GOARCH=386 go build -ldflags="-s -w"
