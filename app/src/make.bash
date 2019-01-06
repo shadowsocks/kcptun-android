479f70ab672db0adba2b5b7f601ab5cf5baa6c53
Mygod <contact-github@mygod.be>
2019-01-06 20:28:57 +0800

    Try to upgrade to go1.11

--- app/src/make.bash
+++ app/src/make.bash
@@ -5,88 +5,50 @@ function try () {
 }
 
 [ -z "$ANDROID_NDK_HOME" ] && ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle
+TOOLCHAIN=$(find $ANDROID_NDK_HOME/toolchains/llvm/prebuilt/* -maxdepth 1 -type d -print -quit)/bin
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MIN_API=$1
 TARGET=$DIR/bin
-DEPS=$(pwd)/.deps
 
-ANDROID_ARM_TOOLCHAIN=$DEPS/android-toolchain-${MIN_API}-arm
-ANDROID_ARM64_TOOLCHAIN=$DEPS/android-toolchain-${MIN_API}-arm64
-ANDROID_X86_TOOLCHAIN=$DEPS/android-toolchain-${MIN_API}-x86
+try mkdir -p $TARGET/armeabi-v7a $TARGET/x86 $TARGET/arm64-v8a $TARGET/x86_64
 
-ANDROID_ARM_CC=$ANDROID_ARM_TOOLCHAIN/bin/arm-linux-androideabi-clang
-ANDROID_ARM_STRIP=$ANDROID_ARM_TOOLCHAIN/bin/arm-linux-androideabi-strip
-
-ANDROID_ARM64_CC=$ANDROID_ARM64_TOOLCHAIN/bin/aarch64-linux-android-clang
-ANDROID_ARM64_STRIP=$ANDROID_ARM64_TOOLCHAIN/bin/aarch64-linux-android-strip
-
-ANDROID_X86_CC=$ANDROID_X86_TOOLCHAIN/bin/i686-linux-android-clang
-ANDROID_X86_STRIP=$ANDROID_X86_TOOLCHAIN/bin/i686-linux-android-strip
-
-try mkdir -p $DEPS $TARGET/armeabi-v7a $TARGET/x86 $TARGET/arm64-v8a
-
-if [ ! -d "$ANDROID_ARM_TOOLCHAIN" ]; then
-    echo "Make standalone toolchain for ARM arch"
-    $ANDROID_NDK_HOME/build/tools/make_standalone_toolchain.py --arch arm \
-        --api $MIN_API --install-dir $ANDROID_ARM_TOOLCHAIN
-fi
-
-if [ ! -d "$ANDROID_ARM64_TOOLCHAIN" ]; then
-    echo "Make standalone toolchain for ARM64 arch"
-    $ANDROID_NDK_HOME/build/tools/make_standalone_toolchain.py --arch arm64 \
-        --api 21 --install-dir $ANDROID_ARM64_TOOLCHAIN
-fi
-
-if [ ! -d "$ANDROID_X86_TOOLCHAIN" ]; then
-    echo "Make standalone toolchain for X86 arch"
-    $ANDROID_NDK_HOME/build/tools/make_standalone_toolchain.py --arch x86 \
-        --api $MIN_API --install-dir $ANDROID_X86_TOOLCHAIN
-fi
-
-if [ ! -d "$DIR/go/bin" ]; then
-    echo "Build the custom go"
-
-    pushd $DIR/go/src
-    try ./make.bash
-    popd
-fi
-
-export GOROOT=$DIR/go
-export GOPATH=$DEPS/gopath
-export GOBIN=$GOPATH/bin
-mkdir -p $GOBIN
-export PATH=$GOROOT/bin:$PATH
+export GOPATH=$DIR
 
 pushd $DIR/kcptun/client
 
 if [ ! -f "$TARGET/armeabi-v7a/libkcptun.so" ] || [ ! -f "$TARGET/arm64-v8a/libkcptun.so" ] ||
-   [ ! -f "$TARGET/x86/libkcptun.so" ]; then
+   [ ! -f "$TARGET/x86/libkcptun.so" ] || [ ! -f "$TARGET/x86_64/libkcptun.so" ]; then
 
     echo "Get dependences for kcptun"
-    go get -u github.com/xtaci/kcp-go
-    go get -u github.com/xtaci/smux
-    go get
+    go get -v
 
     echo "Cross compile kcptun for arm"
-    if [ ! -f "$TARGET/armeabi-v7a/liboverture.so" ]; then
-        try env CGO_ENABLED=1 CC=$ANDROID_ARM_CC GOOS=android GOARCH=arm GOARM=7 go build -ldflags="-s -w"
-        try $ANDROID_ARM_STRIP client
+    if [ ! -f "$TARGET/armeabi-v7a/libkcptun.so" ]; then
+        try env CGO_ENABLED=1 CC=$TOOLCHAIN/armv7a-linux-androideabi${MIN_API}-clang GOOS=android GOARCH=arm GOARM=7 go build -ldflags="-s -w"
+        try $TOOLCHAIN/arm-linux-androideabi-strip client
         try mv client $TARGET/armeabi-v7a/libkcptun.so
     fi
 
     echo "Cross compile kcptun for arm64"
-    if [ ! -f "$TARGET/arm64-v8a/liboverture.so" ]; then
-        try env CGO_ENABLED=1 CC=$ANDROID_ARM64_CC GOOS=android GOARCH=arm64 go build -ldflags="-s -w"
-        try $ANDROID_ARM64_STRIP client
+    if [ ! -f "$TARGET/arm64-v8a/libkcptun.so" ]; then
+        try env CGO_ENABLED=1 CC=$TOOLCHAIN/aarch64-linux-android${MIN_API}-clang GOOS=android GOARCH=arm64 go build -ldflags="-s -w"
+        try $TOOLCHAIN/aarch64-linux-android-strip client
         try mv client $TARGET/arm64-v8a/libkcptun.so
     fi
 
     echo "Cross compile kcptun for x86"
-    if [ ! -f "$TARGET/x86/liboverture.so" ]; then
-        try env CGO_ENABLED=1 CC=$ANDROID_X86_CC GOOS=android GOARCH=386 go build -ldflags="-s -w"
-        try $ANDROID_X86_STRIP client
+    if [ ! -f "$TARGET/x86/libkcptun.so" ]; then
+        try env CGO_ENABLED=1 CC=$TOOLCHAIN/i686-linux-android${MIN_API}-clang GOOS=android GOARCH=386 go build -ldflags="-s -w"
+        try $TOOLCHAIN/i686-linux-android-strip client
         try mv client $TARGET/x86/libkcptun.so
+    fi
+
+    echo "Cross compile kcptun for x86_64"
+    if [ ! -f "$TARGET/x86_64/libkcptun.so" ]; then
+        try env CGO_ENABLED=1 CC=$TOOLCHAIN/x86_64-linux-android${MIN_API}-clang GOOS=android GOARCH=amd64 go build -ldflags="-s -w"
+        try $TOOLCHAIN/x86_64-linux-android-strip client
+        try mv client $TARGET/x86_64/libkcptun.so
     fi
 
     popd
