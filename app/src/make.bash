c74acab392d9d1c61620630bcb658189860b146f
luvjoey1996 <luvjoey1996@gmail.com>
2022-11-20 13:20:11 +0800

    update kcptun

--- app/src/make.bash
+++ app/src/make.bash
@@ -3,7 +3,7 @@
 function try () {
 "$@" || exit -1
 }
-
+ANDROID_NDK_HOME=/opt/android/sdk/ndk/21.4.7075529
 [ -z "$ANDROID_NDK_HOME" ] && ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle
 TOOLCHAIN=$(find $ANDROID_NDK_HOME/toolchains/llvm/prebuilt/* -maxdepth 1 -type d -print -quit)/bin
 
@@ -22,32 +22,35 @@ if [ ! -f "$TARGET/armeabi-v7a/libkcptun.so" ] || [ ! -f "$TARGET/arm64-v8a/libk
 
     echo "Get dependences for kcptun"
     go get -v
-
     echo "Cross compile kcptun for arm"
     if [ ! -f "$TARGET/armeabi-v7a/libkcptun.so" ]; then
         try env CGO_ENABLED=1 CC=$TOOLCHAIN/armv7a-linux-androideabi${MIN_API}-clang GOOS=android GOARCH=arm GOARM=7 go build -ldflags="-s -w"
-        try $TOOLCHAIN/arm-linux-androideabi-strip client
+        strip_exe=arm-linux-androideabi-strip
+        try $TOOLCHAIN/${strip_exe} client
         try mv client $TARGET/armeabi-v7a/libkcptun.so
     fi
 
     echo "Cross compile kcptun for arm64"
     if [ ! -f "$TARGET/arm64-v8a/libkcptun.so" ]; then
         try env CGO_ENABLED=1 CC=$TOOLCHAIN/aarch64-linux-android${MIN_API}-clang GOOS=android GOARCH=arm64 go build -ldflags="-s -w"
-        try $TOOLCHAIN/aarch64-linux-android-strip client
+        strip_exe=aarch64-linux-android-strip
+        try $TOOLCHAIN/${strip_exe} client
         try mv client $TARGET/arm64-v8a/libkcptun.so
     fi
 
     echo "Cross compile kcptun for 386"
     if [ ! -f "$TARGET/x86/libkcptun.so" ]; then
         try env CGO_ENABLED=1 CC=$TOOLCHAIN/i686-linux-android${MIN_API}-clang GOOS=android GOARCH=386 go build -ldflags="-s -w"
-        try $TOOLCHAIN/i686-linux-android-strip client
+        strip_exe=i686-linux-android-strip
+        try $TOOLCHAIN/${strip_exe} client
         try mv client $TARGET/x86/libkcptun.so
     fi
 
     echo "Cross compile kcptun for amd64"
     if [ ! -f "$TARGET/x86_64/libkcptun.so" ]; then
         try env CGO_ENABLED=1 CC=$TOOLCHAIN/x86_64-linux-android${MIN_API}-clang GOOS=android GOARCH=amd64 go build -ldflags="-s -w"
-        try $TOOLCHAIN/x86_64-linux-android-strip client
+        strip_exe=x86_64-linux-android-strip
+        try $TOOLCHAIN/${strip_exe} client
         try mv client $TARGET/x86_64/libkcptun.so
     fi
 
